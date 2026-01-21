98f8bec2fe359e94ddfea5b90a2e982d14b24196
Daniel99808 <danielfonsecamonteiro07@gmail.com>
2026-01-21 20:30:51 -0300

    mudança de cores para o rosa

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Phone, MapPin, Clock } from "lucide-react";
-import heroImage from "@/assets/hero-physiotherapy.jpg";
+import heroImage from "@/assets/hero.png";
 
 const Hero = () => {
   const openWhatsApp = () => {
